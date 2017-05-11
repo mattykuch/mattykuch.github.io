f69ce602a78d9b206bacd7eddd5e5abce3774f56
mattykuch <mattykuch@users.noreply.github.com>
2017-05-11 22:49:05 +0300

    e

--- nmscoldrooms/coldrooms.js
+++ nmscoldrooms/coldrooms.js
@@ -3,7 +3,7 @@ var itemSize = 22,
       margin = {top: 20, right: 20, bottom: 20, left: 20};
       
   var width = 450 - margin.right - margin.left,
-      height = 700 - margin.top - margin.bottom;
+      height = 550 - margin.top - margin.bottom;
 
   var formatDate = d3.time.format("%Y-%m-%d");
 
@@ -11,15 +11,17 @@ var itemSize = 22,
 
     var data = response.map(function( item ) {
         var newItem = {};
-        newItem.country = item.Name;
-        newItem.product = item.Month;
+        newItem.room = item.Name;
+        newItem.month = item.Month;
         newItem.value = item.MKT;
+        newItem.min = item.Min;
+        newItem.max = item.Max;
 
         return newItem;
     })
 
-    var x_elements = d3.set(data.map(function( item ) { return item.product; } )).values(),
-        y_elements = d3.set(data.map(function( item ) { return item.country; } )).values();
+    var x_elements = d3.set(data.map(function( item ) { return item.month; } )).values(),
+        y_elements = d3.set(data.map(function( item ) { return item.room; } )).values();
 
     var xScale = d3.scale.ordinal()
         .domain(x_elements)
@@ -44,10 +46,61 @@ var itemSize = 22,
         .orient("left");
 
     var colorScale = d3.scale.threshold()
-        .domain([2, 8])
-        .range(["#2980B9", "#27AE60", "#fd4000"]); //Blue (Freeze Excursion), Green (Within Range), Red (Heat Excursion)
+        .domain([-100, 2, 3 , 3.5 , 4 , 4.5 , 5 , 5.5, 6.5 , 8, 100 ]) 
+        //.domain(d3.extent(data, function(d) { return value; }))
+        .range(["#2c7bb6","#2c7bb6", "#00a6ca", "#00ccbc", "#90eb9d", "#ffff8c", "#f9d057", "#f29e2e", "#e76818", "#d7191c", "#d7191c"]); //Blue (Freeze Excursion), Green (Within Range), Red (Heat Excursion)
 
-    var svg = d3.select('.coldmap')
+    // Configuring Tooltip
+    // Logic to handle hover event when its firedup
+    var boxon = function(d) {
+    /*      console.log('d', d, 'event', event);*/
+          var div = document.getElementById('boxtip');
+          div.style.left = event.pageX + 'px';
+          div.style.top = event.pageY + 'px';
+
+          
+          //Fill white to highlight
+          d3.select(this)
+            .style("fill", "white");
+
+          //Show the boxtip
+          d3.select("#boxtip")
+            .style("opacity", 1);
+
+          //Populate Room Name in boxtip
+          d3.select("#boxtip .Name")
+            .text(d.room);
+
+          //Populate Min temp in boxtip
+          d3.select("#boxtip .Min")
+            .text("Min:" + " " + d.min);
+
+          //Populate Max temp in boxtip
+          d3.select("#boxtip .Max")
+            .text("Max:" + " " + d.max);
+
+          //Populate MKT in boxtip
+          d3.select("#boxtip .MKT")
+            .text("MKT:" + " " + d.value); 
+    }
+
+    var boxout = function(d) {
+
+      //Restore original fill
+      d3.select(this)
+        .style("fill", function(d) {
+              d3.select(this)
+                  .attr('fill', function(d) { return colorScale(d.value); });
+          });
+
+      //Hide the boxtip
+      d3.select("#boxtip")
+        .style("opacity", 0);
+
+    }    
+
+
+    var svg = d3.select('#coldmap')
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -60,9 +113,11 @@ var itemSize = 22,
         .attr('class', 'cell')
         .attr('width', cellSize)
         .attr('height', cellSize)
-        .attr('y', function(d) { return yScale(d.country); })
-        .attr('x', function(d) { return xScale(d.product); })
-        .attr('fill', function(d) { return colorScale(d.value); });
+        .attr('y', function(d) { return yScale(d.room); })
+        .attr('x', function(d) { return xScale(d.month); })
+        .attr('fill', function(d) { return colorScale(d.value); })
+        .on('mouseover', boxon)
+        .on('mouseout', boxout);
 
     svg.append("g")
         .attr("class", "y axis")
@@ -81,4 +136,89 @@ var itemSize = 22,
         .attr("transform", function (d) {
             return "rotate(-65)";
         });
+
+    //Title label
+    svg.append("g")
+        .append("text")
+        .attr("class", "title")
+        .attr("text-anchor", "middle")
+        .style("font-size", 22 + "px")
+        .attr("x", 5)
+        .attr("y", -100)
+        .text("Cold Rooms");
+
+    //Set up Month axis label
+    svg.append("g")
+        .append("text")
+        .attr("class", "x title")
+        .attr("text-anchor", "end")
+        .attr("x", 200)
+        .attr("y", -50)
+        .text("Month of the Year");
+
+    // Coldrooms label
+    svg.append("text")
+      .attr("class", "y title")
+      .attr("text-anchor", "end")
+      .attr("x", -height/5 )
+      .attr("y", -75)
+      .attr("dy", "0.75em")
+      .attr("transform", "rotate(-90)")
+      .text("Cold Rooms");
+
+
+    //Configuring linear Gradient  
+
+    //Append a defs (for definition) element to your SVG
+    var defs = svg.append("defs");
+
+    //Append a linearGradient element to the defs and give it a unique id
+    var linearGradient = defs.append("linearGradient")
+        .attr("id", "linear-gradient");
+
+    //Append multiple color stops by using D3's data/enter step
+    linearGradient.selectAll("stop") 
+        .data([                             
+            {offset: "0%", color: "#2c7bb6"}, 
+            {offset: "12.5%", color: "#00a6ca"},  
+            {offset: "25%", color: "#00ccbc"}, 
+            {offset: "37.5%", color: "#90eb9d"}, 
+            {offset: "50%", color: "#ffff8c"}, 
+            {offset: "62.5%", color: "#f9d057"}, 
+            {offset: "75%", color: "#f29e2e"},      
+            {offset: "87.5%", color: "#e76818"},        
+            {offset: "100%", color: "#d7191c"}    
+          ])                  
+        .enter().append("stop")
+        .attr("offset", function(d) { return d.offset; })   
+        .attr("stop-color", function(d) { return d.color; });
+
+    //Draw the rectangle and fill with gradient
+    svg.append("rect")
+        .attr('class', 'scaleGradient')
+        .attr("width", 300)
+        .attr("height", 20)
+        .attr("x",0)
+        .attr("y", height - 30)
+        .style("fill", "url(#linear-gradient)");
+
+    //Low Temperature label - 2C
+    svg.append("g")
+        .append("text")
+        .attr("class", "lowtemp")
+        .attr("text-anchor", "end")
+        .attr("x", 0)
+        .attr("y", height )
+        .text("2C");
+
+    //High Temperature label - 8C
+    svg.append("g")
+        .append("text")
+        .attr("class", "hightemp")
+        .attr("text-anchor", "end")
+        .attr("x", 318)
+        .attr("y", height )
+        .text("8C");
+
+
   });
